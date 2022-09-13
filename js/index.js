c266546d69bba8cdc9e5c2a3d893bc4f7aef0287
Michael M <80980887+Michael-Swordys@users.noreply.github.com>
2022-09-13 18:03:46 -0300

    att

--- js/index.js
+++ js/index.js
@@ -17,3 +17,19 @@ function ShowPassword()
         password.type = 'password';
     }
 }
+function validarEmail(){
+    var email = document.querySelector('#email');
+    var error = document.querySelector('#error-email');
+    
+    if(!email.checkValidity()){
+      error.innerHTML = "Email invalido";  
+    }
+     
+  }
+  
+  function redefinirMsg(){
+    var error = document.querySelector('#error-email');
+    if (error.innerHTML == "Email invalido"){
+      error.innerHTML = "";
+    }
+  }
